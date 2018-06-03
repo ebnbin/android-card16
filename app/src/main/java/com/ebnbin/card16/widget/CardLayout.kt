2d1eb277e2e491ead58e1c2387d49d0a329fbf92
Ebn Zhang <ebnbin@gmail.com>
2018-06-03 22:35:49 +0800

    Add animInOut for CardLayout

--- app/src/main/java/com/ebnbin/card16/widget/CardLayout.kt
+++ app/src/main/java/com/ebnbin/card16/widget/CardLayout.kt
@@ -10,6 +10,7 @@ import android.support.v7.widget.CardView
 import android.util.AttributeSet
 import android.view.View
 import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.AccelerateInterpolator
 import android.view.animation.DecelerateInterpolator
 import android.widget.Button
 import com.ebnbin.eb.util.dp
@@ -20,7 +21,26 @@ import com.ebnbin.eb.util.dp
 class CardLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
         CardView(context, attrs, defStyleAttr) {
     init {
-        setOnClickListener { animIn(true, false, Degrees.D90, 400L, 100L) }
+        setOnClickListener {
+            animInOut(
+                    isIn = false,
+                    isHorizontal = true,
+                    isClockwise = false,
+                    degrees = Degrees.D90,
+                    alphaRotationXYDuration = 400L,
+                    elevationDuration = 100L,
+                    startDelay = 0L,
+                    onEnd = {
+                        animInOut(
+                                isIn = true,
+                                isHorizontal = true,
+                                isClockwise = false,
+                                degrees = Degrees.D90,
+                                alphaRotationXYDuration = 400L,
+                                elevationDuration = 100L,
+                                startDelay = 0L)
+                    })
+        }
     }
 
     private val button = Button(this.context).apply {
@@ -46,11 +66,23 @@ class CardLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet
     }
 
     /**
-     * 翻转出现动画.
+     * 翻转出现或消失动画.
+     *
+     * 如果为出现动画:
      *
      * 1. 透明度从 0f 到 1f, 同时翻转. 插值器为 [DecelerateInterpolator].
      *
      * 2. 高度从 [MAX_ELEVATION] 到 [DEF_ELEVATION]. 插值器为 [AccelerateDecelerateInterpolator].
+     *
+     * 3. 动画完成后显示视图.
+     *
+     * 如果为消失动画:
+     *
+     * 1. 高度从 [DEF_ELEVATION] 到 [MAX_ELEVATION]. 插值器为 [AccelerateDecelerateInterpolator].
+     *
+     * 2. 透明度从 1f 到 0f, 同时翻转. 插值器为 [AccelerateInterpolator].
+     *
+     * 3. 动画完成后隐藏视图.
      *
      * 卡片旋转到背面时隐藏子视图. 动画时不可点击.
      *
@@ -62,16 +94,41 @@ class CardLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet
      *
      * @param alphaRotationXYDuration 淡入和翻转动画时长.
      *
-     * @param elevationDuration 降低高度动画时长.
+     * @param elevationDuration 高度动画时长.
+     *
+     * @param startDelay 动画延时.
+     *
+     * @param onStart 动画开始回调.
+     *
+     * @param onEnd 动画结束回调.
+     *
+     * @param onCardFront 卡片翻转到正面回调.
+     *
+     * @param onCardBack 卡片翻转到背面回调.
      */
-    private fun animIn(isHorizontal: Boolean, isClockwise: Boolean, degrees: Degrees, alphaRotationXYDuration: Long,
-            elevationDuration: Long) {
-        val alphaFromValue = 0f
-        val alphaObjectAnimator = ObjectAnimator.ofFloat(this, "alpha", alphaFromValue, 1f)
+    private fun animInOut(
+            isIn: Boolean,
+            isHorizontal: Boolean,
+            isClockwise: Boolean,
+            degrees: Degrees,
+            alphaRotationXYDuration: Long,
+            elevationDuration: Long,
+            startDelay: Long,
+            onStart: (() -> Unit)? = null,
+            onEnd: (() -> Unit)? = null,
+            onCardFront: (() -> Unit)? = null,
+            onCardBack: (() -> Unit)? = null) {
+        val alphaFromValue = if (isIn) 0f else 1f
+        val alphaToValue = if (isIn) 1f else 0f
+        val alphaObjectAnimator = ObjectAnimator.ofFloat(this, "alpha", alphaFromValue, alphaToValue)
 
         val rotationXYPropertyName = if (isHorizontal) "rotationY" else "rotationX"
-        val rotationXYFromValue = (if (isClockwise) 1 else -1) * degrees.ordinal * 90f
-        val rotationXYObjectAnimator = ObjectAnimator.ofFloat(this, rotationXYPropertyName, rotationXYFromValue, 0f)
+        val rotationXYFromClockwiseSign = if (isClockwise) 1 else -1
+        val rotationXYFromValue = if (isIn) rotationXYFromClockwiseSign * degrees.ordinal * 90f else 0f
+        val rotationXYToClockwiseSign = -rotationXYFromClockwiseSign
+        val rotationXYToValue = if (isIn) 0f else rotationXYToClockwiseSign * degrees.ordinal * 90f
+        val rotationXYObjectAnimator = ObjectAnimator.ofFloat(this, rotationXYPropertyName, rotationXYFromValue,
+                rotationXYToValue)
         val rotationXYAnimatorUpdateListener = object : ValueAnimator.AnimatorUpdateListener {
             private var isFront: Boolean? = null
 
@@ -84,12 +141,16 @@ class CardLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet
                         if (isFront != false) {
                             isFront = false
                             getChildAt(0)?.visibility = View.GONE
+
+                            onCardBack?.invoke()
                         }
                     }
                     else -> {
                         if (isFront != true) {
                             isFront = true
                             getChildAt(0)?.visibility = View.VISIBLE
+
+                            onCardFront?.invoke()
                         }
                     }
                 }
@@ -99,15 +160,20 @@ class CardLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet
         val alphaRotationXYAnimatorSet = AnimatorSet()
         alphaRotationXYAnimatorSet.playTogether(alphaObjectAnimator, rotationXYObjectAnimator)
         alphaRotationXYAnimatorSet.duration = alphaRotationXYDuration
-        alphaRotationXYAnimatorSet.interpolator = DecelerateInterpolator()
+        alphaRotationXYAnimatorSet.interpolator = if (isIn) DecelerateInterpolator() else AccelerateInterpolator()
 
-        val elevationFromValue = MAX_ELEVATION
-        val elevationObjectAnimator = ObjectAnimator.ofFloat(this, "elevation", elevationFromValue, DEF_ELEVATION)
+        val elevationFromValue = if (isIn) MAX_ELEVATION else DEF_ELEVATION
+        val elevationToValue = if (isIn) DEF_ELEVATION else MAX_ELEVATION
+        val elevationObjectAnimator = ObjectAnimator.ofFloat(this, "elevation", elevationFromValue, elevationToValue)
         elevationObjectAnimator.duration = elevationDuration
         elevationObjectAnimator.interpolator = AccelerateDecelerateInterpolator()
 
         val animatorSet = AnimatorSet()
-        animatorSet.playSequentially(alphaRotationXYAnimatorSet, elevationObjectAnimator)
+        if (isIn)
+            animatorSet.playSequentially(alphaRotationXYAnimatorSet, elevationObjectAnimator)
+        else
+            animatorSet.playSequentially(elevationObjectAnimator, alphaRotationXYAnimatorSet)
+        animatorSet.startDelay = startDelay
         animatorSet.addListener(object : AnimatorListenerAdapter() {
             override fun onAnimationStart(animation: Animator?) {
                 super.onAnimationStart(animation)
@@ -121,6 +187,8 @@ class CardLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet
                 alpha = alphaFromValue
                 if (isHorizontal) rotationY = rotationXYFromValue else rotationX = rotationXYFromValue
                 elevation = elevationFromValue
+
+                onStart?.invoke()
             }
 
             override fun onAnimationEnd(animation: Animator?) {
@@ -131,6 +199,10 @@ class CardLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet
                 rotationXYObjectAnimator.removeUpdateListener(rotationXYAnimatorUpdateListener)
 
                 isClickable = true
+
+                visibility = if (isIn) View.VISIBLE else View.GONE
+
+                onEnd?.invoke()
             }
         })
         animatorSet.start()
