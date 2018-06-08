9bdd4e7cdd69c7a7487264c64bd675ba4356b440
Ebn Zhang <ebnbin@gmail.com>
2018-06-08 18:57:27 +0800

    Delete BaseCardLayout

--- app/src/main/java/com/ebnbin/card16/widget/BaseCard.kt
+++ app/src/main/java/com/ebnbin/card16/widget/BaseCard.kt
@@ -4,7 +4,10 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.content.Context
+import android.support.v7.widget.CardView
+import android.view.View
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.animation.AccelerateInterpolator
 import android.view.animation.DecelerateInterpolator
@@ -12,7 +15,7 @@ import android.view.animation.DecelerateInterpolator
 /**
  * 基础卡片.
  */
-abstract class BaseCard(context: Context) : BaseCardLayout(context) {
+abstract class BaseCard(context: Context) : CardView(context) {
     protected val card16Layout get() = parent as Card16Layout
 
     /**
@@ -208,25 +211,96 @@ abstract class BaseCard(context: Context) : BaseCardLayout(context) {
     }
 
     /**
+     * 卡片正面或反面.
+     */
+    var isCardFront = true
+        private set(value) {
+            if (field == value) return
+            field = value
+            onCardFrontBack(field)
+        }
+
+    /**
+     * 卡片正反面改变回调.
+     */
+    protected open fun onCardFrontBack(isFront: Boolean) {
+        // TODO
+        for (index in 0 until childCount) {
+            getChildAt(index).visibility = if (isFront) View.VISIBLE else View.GONE
+        }
+    }
+
+    /**
+     * 翻转动画更新监听器. 监听卡片正反面改变.
+     *
+     * @param isClockwise 翻转方向顺时针或逆时针.
+     */
+    protected inner class CardFrontBackAnimatorUpdateListener(private val isClockwise: Boolean) :
+            ValueAnimator.AnimatorUpdateListener {
+        override fun onAnimationUpdate(animation: ValueAnimator?) {
+            val rotation = animation?.animatedValue as? Float? ?: return
+            val validRotation = (rotation % 360f + 360f) % 360f
+            isCardFront = when (validRotation) {
+                90f -> isClockwise
+                270f -> !isClockwise
+                in 90f..270f -> false
+                else -> true
+            }
+        }
+    }
+
+    /**
+     * 动画开始时显示当前视图. 动画结束时递归移除全部动画监听器.
      * 动画开始时设置全部卡片和大卡片不可点击. 动画结束时设置全部卡片和大卡片可点击.
      */
-    protected open inner class CardAnimatorListener(onStart: ((Animator) -> Unit)? = null,
-            onEnd: ((Animator) -> Unit)? = null) : CardLayoutAnimatorListener(onStart, onEnd) {
-        override fun onStart(animator: Animator) {
-            super.onStart(animator)
+    protected open inner class CardAnimatorListener(
+            private val onStart: ((Animator) -> Unit)? = null,
+            private val onEnd: ((Animator) -> Unit)? = null) : Animator.AnimatorListener {
+        final override fun onAnimationStart(animation: Animator?) {
+            animation ?: return
+
+            visibility = View.VISIBLE
 
             // TODO
             card16Layout.cards { it.isClickable = false }
             card16Layout.bigCard.isClickable = false
+
+            onStart(animation)
+            onStart?.invoke(animation)
         }
 
-        override fun onEnd(animator: Animator) {
-            super.onEnd(animator)
+        protected open fun onStart(animator: Animator) = Unit
+
+        final override fun onAnimationEnd(animation: Animator?) {
+            animation ?: return
+
+            removeAllListeners(animation)
 
             // TODO
             card16Layout.cards { it.isClickable = true }
             card16Layout.bigCard.isClickable = true
+
+            onEnd(animation)
+            onEnd?.invoke(animation)
         }
+
+        /**
+         * 递归移除全部动画监听器.
+         */
+        private fun removeAllListeners(animator: Animator) {
+            animator.removeAllListeners()
+            if (animator is ValueAnimator) {
+                animator.removeAllUpdateListeners()
+            } else if (animator is AnimatorSet) {
+                animator.childAnimations.forEach { removeAllListeners(it) }
+            }
+        }
+
+        protected open fun onEnd(animator: Animator) = Unit
+
+        final override fun onAnimationCancel(animation: Animator?) = Unit
+
+        final override fun onAnimationRepeat(animation: Animator?) = Unit
     }
 
     companion object {
